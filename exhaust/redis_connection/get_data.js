bb243695d129c7978fcd01503b54afcb956209bb
jeffreyb7 <jwbishop09@gmail.com>
2022-02-27 21:43:54 -0600

    Finished basic data endpt and templates

--- exhaust/redis_connection/get_data.js
+++ exhaust/redis_connection/get_data.js
@@ -11,9 +11,38 @@ async function getAttributes() {
 		if (err) throw err;
 	});
 
-	dbConnection.quit();
+	await dbConnection.quit();
 
 	return allAttrs;
 };
 
-module.exports = getAttributes;
+async function getRanks(attribute) {
+    const dbConnection = await makeConnection();
+    
+    /* The following command does not work:
+        dbConnection.ZRANGE(attribute, 0, -1, {WITHSCORES: true})
+    This is why I'm using sendCommand
+    Consider submitting this to node-redis as an issue?
+    */
+    const rawRanks = await dbConnection.sendCommand(['ZRANGE', attribute, 0, -1, 'WITHSCORES'], (err, reply) => {
+        if (err) throw err;
+    });
+
+    await dbConnection.quit();
+
+    const stdRanks = new Object(); 
+    
+    while (rawRanks.length > 0) {
+        let rankPair = rawRanks.splice(0, 2);
+        let carModel = rankPair[0].replace(/_/g, ' ');
+        let carValue = parseFloat(rankPair[1]);
+        stdRanks[carModel] = carValue;
+    };
+    
+    return stdRanks;
+};
+
+module.exports = {
+    getAttributes,
+    getRanks
+};
--- exhaust/routes/chooseView.js
+++ exhaust/routes/chooseView.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-var getAttributes = require('../redis_connection/get_data.js');
+var carData = require('../redis_connection/get_data.js');
 
 /* Show attributes to explore */
 router.get('/', function(req, res) {
-	getAttributes().then(function(attributes) {
-		res.render('attributes');
-	});
+	carData.getAttributes().then(function(attributes) {
+	    res.render('attributes', { attributes: attributes });
+    });
+});
+
+
+router.get('/:attribute', function(req, res) {
+    carData.getRanks(req.params.attribute).then(function(data) {
+        res.send(data);
+    });
 });
 
 module.exports = router;
